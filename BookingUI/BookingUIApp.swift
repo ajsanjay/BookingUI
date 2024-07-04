01a483583799d498bb1d94f4454f0b4bd7001bfb
Jaya Sabeen <jayasabeen@Jayas-MacBook-Pro.local>
2024-07-04 10:20:18 +0530

    Project Initialized

---
+++ BookingUI/BookingUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingUIApp.swift
+//  BookingUI
+//
+//  Created by Jaya Sabeen on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BookingUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
